c5d551b25ff4318327a8d8153c68e4bd78264dab
Yaron <yaronavraham600@gmail.com>
2021-02-07 18:21:25 +0200

    atomic+storybook_create enums and models for button

--- budget-app/.storybook/main.js
+++ budget-app/.storybook/main.js
@@ -8,4 +8,6 @@ module.exports = {
       propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
     },
   },
+  stories: ['../src//*.stories.mdx', '../src//*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app'],
 };
